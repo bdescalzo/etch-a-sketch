dd1e2ca85831512488dc7e7f8d54e88e3121200c
Beñat Descalzo Alcuaz <guaibenat@gmail.com>
2022-08-12 16:02:43 +0200

    Add reset functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Grab elements from DOM
 const containerEl = document.querySelector(".grid-container");
+const resetBtn = document.querySelector(".reset-btn");
 
 let squaresPerRow, gridSize;
 
@@ -14,19 +15,34 @@ function resizeGrid(squaresAmount) {
   containerEl.style.gridTemplateAreas = `repeat(${squaresPerRow}, 1fr)`;
 }
 
+// Populate the grid with squares that color themselves
 function populateGrid() {
   for (let i = 0; i < gridSize; i++) {
     // Create the square
     const gridSquareEl = document.createElement("div");
     gridSquareEl.classList.add("grid-square");
 
-    // When hovering, change color to black
-    gridSquareEl.addEventListener("mouseover", (e) => {
-      e.target.style.backgroundColor = "black";
-    });
+    // When clicking, change color to black
+    gridSquareEl.addEventListener("mouseover", colorSquare);
     containerEl.appendChild(gridSquareEl);
   }
 }
 
+// Resets, recreates and populates the grid with the specified size
+function resetGrid() {
+  // Empty the grid
+  containerEl.textContent = "";
+
+  // Ask for the size and repopulate
+  const size = prompt("How many squares per row?");
+  resizeGrid(size);
+  populateGrid();
+}
+
+// When a square is clicked, colour it with the current colour
+function colorSquare(e) {
+  e.target.style.backgroundColor = "black";
+}
+resetBtn.addEventListener("click", resetGrid);
 resizeGrid(16);
 populateGrid();
