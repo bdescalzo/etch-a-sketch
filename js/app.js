04f41b63be625b16321700137e29fdc35a999d92
Beñat Descalzo Alcuaz <guaibenat@gmail.com>
2022-08-12 13:29:34 +0200

    Refactor code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
 // Grab elements from DOM
 const containerEl = document.querySelector(".grid-container");
 
-const squareAmount = 3;
+let squaresPerRow, gridSize;
 
-containerEl.style.gridTemplateColumns = `repeat(${squareAmount}, 1fr)`;
-containerEl.style.gridTemplateAreas = `repeat(${squareAmount}, 1fr)`;
+// Resizes the grid depending on the squares per row that the user wants
+function resizeGrid(squaresAmount) {
+  squaresPerRow = parseInt(squaresAmount);
 
-for (let i = 0; i < squareAmount * squareAmount; i++) {
-  // Create the square
-  const gridSquareEl = document.createElement("div");
-  gridSquareEl.classList.add("grid-square");
+  gridSize = squaresPerRow ** 2;
 
-  containerEl.appendChild(gridSquareEl);
+  // Resize grid according to the squares per row
+  containerEl.style.gridTemplateColumns = `repeat(${squaresPerRow}, 1fr)`;
+  containerEl.style.gridTemplateAreas = `repeat(${squaresPerRow}, 1fr)`;
 }
+
+function populateGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    // Create the square
+    const gridSquareEl = document.createElement("div");
+    gridSquareEl.classList.add("grid-square");
+
+    // When hovering, change color to black
+    gridSquareEl.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "black";
+    });
+    containerEl.appendChild(gridSquareEl);
+  }
+}
+
+resizeGrid(16);
+populateGrid();
